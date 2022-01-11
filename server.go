dd29514f35fa80fb5f15b51b8cca5d91c3746363
Jack Davidson <jackmaverickavidson@gmail.com>
2022-01-11 09:17:23 -0600

    updated templating

--- server.go
+++ server.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"log"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	app.Static("/", "./public", fiber.Static{
+		CacheDuration: 1 * time.Microsecond,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World")
+		fmt.Println(c)
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.Context().SetContentType("text/html")
+		tmpl.Execute(c, nil)
+
+		return nil
 	})
 
 	app.Listen(":3000")
