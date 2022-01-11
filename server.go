ae62986a7278889e46ba6c8baef8e2ae9417b401
Jack Davidson <jackmaverickavidson@gmail.com>
2022-01-11 09:10:37 -0600

    initial route

--- server.go
+++ server.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
 }
